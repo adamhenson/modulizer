05447f5ee619930f5e351698a9f2cc41cced7e12
Adam Henson <adamhenson1979@gmail.com>
2016-03-26 16:56:34 -0400

    More tests.

--- test/main.js
+++ test/main.js
@@ -2,10 +2,21 @@
 
 const ASSERT = require('assert');
 const MODULIZER = require('../');
-let modulizer = new MODULIZER(__dirname + '/files');
+
+describe('new Modulizer()', function() {
+
+  it('should instantiate without errors.', function() {
+    let modulizer = new MODULIZER(__dirname + '/files');
+  });
+
+});
  
 describe('Modulizer.initialize()', function() {
-  it('returns 1 when initilized on a directory of 2 JavaScript files in which 1 doesn\'t include an export statement.', function() {
+
+  let modulizer = new MODULIZER(__dirname + '/files');
+
+  it('should return 1 when initilized on a directory of 2 JavaScript files in which 1 doesn\'t include an export statement.', function() {
     ASSERT.equal(modulizer.initialize(), 1);
   });
+
 });
