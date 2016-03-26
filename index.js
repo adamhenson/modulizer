94492fa3054a26cad067fd235981c690fba42d09
Adam Henson <adamhenson1979@gmail.com>
2016-03-26 11:35:47 -0400

    tabs to spaces.

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ const MODULE_NAME = INFO.name;
 
 class ExpressRouteDirectory {
 
-	constructor(directory, options) {
-		if(!directory){
-			throw new Error(`${MODULE_NAME}: constructor(): "directory" argument is undefined.`);
-		}
-		this._directory = directory;
-		// default function arguments still unsupported :(
-		this._options = options || {};
-	}
+  constructor(directory, options) {
+    if(!directory){
+      throw new Error(`${MODULE_NAME}: constructor(): "directory" argument is undefined.`);
+    }
+    this._directory = directory;
+    // default function arguments still unsupported :(
+    this._options = options || {};
+  }
 
-	initialize() {
-		let self = this;
-		FS.readdirSync(self._directory).forEach((file) => {
-	    if(file.indexOf('.js') !== -1) {
-	    	require(self._directory + '/' + file)(self._options);
-	    }
-	  });
-	}
+  initialize() {
+    let self = this;
+    FS.readdirSync(self._directory).forEach((file) => {
+      if(file.indexOf('.js') !== -1) {
+        require(self._directory + '/' + file)(self._options);
+      }
+    });
+  }
 
 }
 
